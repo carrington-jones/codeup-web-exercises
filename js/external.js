a85e79b6d065e2fa2f41aaa72bbb6d105cab60b2
Carrington Jones <carrington.jones47@gmail.com>
2021-03-19 10:58:26 -0500

    updated after instructor walkthrough on alerts and prompts

--- js/external.js
+++ js/external.js
@@ -1,14 +1,26 @@
 "use strict"
 
-var hello = "Hello from external JavaScript";
-
-console.log(hello);
-
+// var hello = "Hello from external JavaScript";
+//
+// console.log(hello);
+//
 //alert("Welcome to my Website!");
-
+//
 // var favoriteColor = prompt("What is your favorite color?");
-
+//
 // alert(favoriteColor + " is my favorite color too!");
+//
+// var littleMermaid = parseFloat(prompt("How many days are you renting the Little Mermaid?"));
+// var brotherBear = parseFloat(prompt("How many days are you renting the Brother Bear"));
+// var hercules = parseFloat(prompt("How many days are you renting the Hercules?"));
+//
+// var rentalRate = parseFloat(prompt("How much is it to rent a movie for 1 day?"));
+//
+// var totalCost = rentalRate * (littleMermaid + brotherBear + hercules);
+//
+// alert("Your total cost is $" + totalCost.toFixed(2));
+
+
 
 // var googleRate = prompt("How much does Google pay you per hour?");
 // parseFloat(googleRate);
@@ -22,13 +34,29 @@ console.log(hello);
 // parseFloat(amazonHours);
 // var facebookHours = prompt("How many hours did you work at Facebook?");
 // parseFloat(facebookHours);
-
+//
 // var totalPay = alert("Your paycheck this week will be: $" + ((googleRate * googleHours) + (amazonRate * amazonHours) + (facebookRate * facebookHours)));
 // console.log(totalPay);
 
-var isThereRoominClass = confirm("If there is room in the class Click ok, if there is not room in the class please hit cancel");
-var openschedule = confirm("Is your schedule free for this class?");
-var canregister = isThereRoominClass && openschedule;
-console.log(canregister)
+// var haveClassAt10 = confirm("Will this interfere with any other classes? Click ok for no and click cancel for yes.");
+// var classFull = confirm("Is this class full? Press ok for no and cancel for yes.");
+// var canRegister = (haveClassAt10 && classFull);
+// alert("You can register for this class: " + (canRegister));
+// console.log(canRegister);
+
+// var offerValid = confirm("Are we still running that promo on fish sticks?");
+// var premiumMember = confirm("Are you a premium member? Please click ok for yes and cancel for no.");
+// var boughtTwo = prompt("How many items did you buy?");
+// parseFloat(boughtTwo);
+//
+// var willGetDiscount = (premiumMember || (boughtTwo >= 2)) && offerValid;
+//
+// if (willGetDiscount === true) {
+//     alert("You will get this discount!")
+// } else{
+//     alert("You will not get the discount")
+// }
+// console.log(willGetDiscount);
+
 
 
