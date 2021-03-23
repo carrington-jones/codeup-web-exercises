266afdd717fbe3e1adc61275a9bf8d6c415f6ac6
Carrington Jones <carrington.jones47@gmail.com>
2021-03-22 21:14:55 -0500

    1st assessment extra exercises 03.21.21

---
+++ js/extra_exercises.js
@@ -0,0 +1,9 @@
+
+1.
+var userName = prompt("What is your name?");
+if (userName.length > 0) {
+    alert("Your name is " + userName + " and that is " + (userName.length) + " letters long!")
+} else {
+    alert("You did not enter your name. Please refresh the page and enter your name")
+}
+
