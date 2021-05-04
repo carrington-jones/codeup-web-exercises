51174651296b131138b6211fdf871e8f3ce84bc9
Carrington Jones <carrington.jones47@gmail.com>
2021-05-04 16:01:34 -0500

    Worked on jQuery Traversing

--- js/jQuery-essential-methods-attributes-and-CSS.js
+++ js/jQuery-essential-methods-attributes-and-CSS.js
@@ -7,3 +7,21 @@ $("dd").toggleClass('invisible');
 $("dt").click(function(){
     $(this).toggleClass("highlight");
 })
+
+$("#yellowButton").click(function(){
+    $("ul").each(function(){
+        $(this).children().last().css("background-color", "yellow");
+    })
+});
+
+$("h3").click(function() {
+    // $("h3").each(function(){
+        $("h3").next().children().css("font-weight", "bold");
+    // });
+});
+
+$("li").click(function(){
+    $("li").each(function(){
+        $(this).parent().children().first().css("color", "blue")
+    })
+});
