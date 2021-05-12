a6e2ff67e1ef365438e62a906e70bff370c61198
Carrington Jones <carrington.jones47@gmail.com>
2021-05-12 12:19:21 -0500

    Worked on weather map project.Various styling still needed

--- js/weather_map.js
+++ js/weather_map.js
@@ -27,7 +27,7 @@ var popup = new mapboxgl.Popup()
 marker.setPopup(popup);
 
 // Code that runs when marker is dropped
-marker.on('dragend', function(){
+marker.on('dragend', function () {
     var markerLngLat = marker.getLngLat();
     var draggedMarkerName = reverseGeocode(markerLngLat, MAPBOX_ACCESS_TOKEN).then(function (results) {
         $("#weatherTitle").html("Weather Forecast for " + results.features[2].place_name)
@@ -47,7 +47,7 @@ marker.on('dragend', function(){
             var Month = date.toLocaleString('en-us', {month: 'long'});
             var numberDate = date.toLocaleString('en-us', {day: 'numeric'});
             html += "<div class='card text-center col-2 m-3 d-flex shadow-lg p-3 mb-5 bg-white rounded'>"
-            html += '<h3>' + Day + '<br> ' + Month + ' ' + numberDate + '</h3>';
+            html += '<h2>' + Day + '</h2>' + '<br> ' + '<h4>' + Month + ' ' + numberDate + '</h4>';
             html += '<p>' + "<img src=http://openweathermap.org/img/w/" + icons + ".png" + ">" + '</p>' //Icons from OpenWeather API
             html += '<p>' + description + '</p>';
             html += '<p>High / Low</p>';
@@ -60,9 +60,9 @@ marker.on('dragend', function(){
     });
 })
 
-
 //Code for flying to user input on Map
-$("#btn").click(function () {
+$("#btn").click(function (e) {
+    e.preventDefault()
     var userInput = $("#userWeatherInput").val();
     geocode(userInput, MAPBOX_ACCESS_TOKEN).then(function (info) {
         var userMarker1 = {
@@ -87,7 +87,7 @@ $("#btn").click(function () {
                 var Month = date.toLocaleString('en-us', {month: 'long'});
                 var numberDate = date.toLocaleString('en-us', {day: 'numeric'});
                 html += "<div class='card text-center col-2 m-3 d-flex shadow-lg p-3 mb-5 bg-white rounded'>"
-                html += '<h3>' + Day + '<br> ' + Month + ' ' + numberDate + '</h3>';
+                html += '<h2>' + Day + '</h2>' + '<br> ' + '<h4>' + Month + ' ' + numberDate + '</h4>';
                 html += '<p>' + "<img src=http://openweathermap.org/img/w/" + icons + ".png" + ">" + '</p>' //Icons from OpenWeather API
                 html += '<p>' + description + '</p>';
                 html += '<p>High / Low</p>';
@@ -100,6 +100,8 @@ $("#btn").click(function () {
         });
     });
 })
+
+
 // var hersheyCoordinates = [40.2859, -76.6502]
 //Initial Starting point. Hershey, PA
 var lat = 40.2859
@@ -128,7 +130,7 @@ function renderWeather(resp) {
         var Month = date.toLocaleString('en-us', {month: 'long'});
         var numberDate = date.toLocaleString('en-us', {day: 'numeric'});
         html += "<div class='card text-center col-2 m-3 d-flex shadow-lg p-3 mb-5 bg-white rounded'>"
-        html += '<h3>' + Day + '<br> ' + Month + ' ' + numberDate + '</h3>';
+        html += '<h2>' + Day + '</h2>' + '<br> ' + '<h4>' + Month + ' ' + numberDate + '</h4>';
         html += '<p>' + "<img src=http://openweathermap.org/img/w/" + icons + ".png" + ">" + '</p>' //Icons from OpenWeather API
         html += '<p>' + description + '</p>';
         html += '<p>High / Low</p>';
@@ -139,6 +141,96 @@ function renderWeather(resp) {
     }
     $('#weatherForecast').html(html);
 }
+
+//Popular Destinations dropdown attempt
+var parisCoordinates = {lat: 48.8566, lng: 2.3522}
+var parisCoordinates2 = [48.8566, 2.3522]
+var tokyoCoordinates = {lat: 35.6762, lng: 139.6503}
+var tokyoCoordinates2 = [35.6762, 139.6503]
+var munichCoordinates = {lat: 48.1351, lng: 11.5820}
+var munichCoordinates2 = [48.1351, 11.5820]
+
+function popularDestination(coordinates2) {
+    $.ajax("https://api.openweathermap.org/data/2.5/onecall?units=imperial&lat=" + coordinates2[0] + "&lon=" + coordinates2[1] + "&exclude=current,hourly,minutely&appid=" + WEATHER_MAP_TOKEN).done(function (data1) {
+        // console.log(data1);//This logs entire weather object
+        var html = "";
+        for (var i = 0; i < 5; i++) {
+            var dailyForecast = data1.daily[i];
+            var icons = dailyForecast.weather[0].icon
+            var description = dailyForecast.weather[0].main
+            var date = new Date(dailyForecast.dt * 1000);
+            var Day = date.toLocaleString('en-us', {weekday: 'long'});
+            var Month = date.toLocaleString('en-us', {month: 'long'});
+            var numberDate = date.toLocaleString('en-us', {day: 'numeric'});
+            html += "<div class='card text-center col-2 m-3 d-flex shadow-lg p-3 mb-5 bg-white rounded'>"
+            html += '<h2>' + Day + '</h2>' + '<br> ' + '<h4>' + Month + ' ' + numberDate + '</h4>';
+            html += '<p>' + "<img src=http://openweathermap.org/img/w/" + icons + ".png" + ">" + '</p>' //Icons from OpenWeather API
+            html += '<p>' + description + '</p>';
+            html += '<p>High / Low</p>';
+            html += '<p>' + Math.round(dailyForecast.temp.max) + 'F / ';
+            html += Math.round(dailyForecast.temp.min) + ' F</p>';
+            html += '<p>' + 'Wind Speed - ' + Math.round(dailyForecast.wind_speed) + ' MPH' + '</p>';
+            html += "</div>"
+        }
+        $('#weatherForecast').html(html);
+    });
+}
+
+function popularDestinationParis() {
+    var popularDestination1 = reverseGeocode(parisCoordinates, MAPBOX_ACCESS_TOKEN).then(function (results) {
+        $("#weatherTitle").html("Weather Forecast for " + results.features[2].place_name)
+        console.log(results)
+    });
+    geocode("Paris", MAPBOX_ACCESS_TOKEN).then(function (info) {
+        var parisCoordinates3 = {
+            lng: info[0],
+            lat: info[1]
+        }
+        marker.setLngLat(parisCoordinates);
+        popup.setHTML("<h5>" + "Paris, France" + "</h5>" + "<br>" + "<p>" + "Paris holds the world record for capital with the least amount of stop signs with the grand total of…0" + "</p>")
+        map.flyTo({center: parisCoordinates3});
+        popularDestination(parisCoordinates2);
+    });
+}
+function popularDestinationTokyo() {
+    var popularDestination1 = reverseGeocode(tokyoCoordinates, MAPBOX_ACCESS_TOKEN).then(function (results) {
+        $("#weatherTitle").html("Weather Forecast for " + results.features[2].place_name)
+        console.log(results)
+    });
+    geocode("Tokyo", MAPBOX_ACCESS_TOKEN).then(function (info) {
+        var tokyoCoordinates3 = {
+            lng: info[0],
+            lat: info[1]
+        }
+        marker.setLngLat(tokyoCoordinates);
+        popup.setHTML("<h5>" + "Tokyo, Japan" + "</h5>" + "<br>" + "<p>" + "Tokyo is the largest metropolitan area in the world" + "</p>")
+        map.flyTo({center: tokyoCoordinates3});
+        popularDestination(tokyoCoordinates2);
+    });
+}
+function popularDestinationMunich() {
+    var popularDestination1 = reverseGeocode(munichCoordinates, MAPBOX_ACCESS_TOKEN).then(function (results) {
+        $("#weatherTitle").html("Weather Forecast for " + results.features[2].place_name)
+        console.log(results)
+    });
+    geocode("Munich", MAPBOX_ACCESS_TOKEN).then(function (info) {
+        var munichCoordinates3 = {
+            lng: info[0],
+            lat: info[1]
+        }
+        marker.setLngLat(munichCoordinates);
+        popup.setHTML("<h5>" + "Munich, Germany" + "</h5>" + "<br>" + "<p>" + "Munich is further north than any major US city." + "</p>");
+        map.flyTo({center: munichCoordinates3});
+        popularDestination(munichCoordinates2);
+    });
+}
+
+
+// var paris = document.getElementById('#paris')
+// paris.addEventListener('click', function(e){
+//     popularDestination(parisCoordinates)
+// })
+
 
 // var today = resp.daily[0]; //Current Date formula
 // var todayDate = new Date(today.dt * 1000);
