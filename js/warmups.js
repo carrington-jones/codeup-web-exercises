91532ab059e8d4dabf0111c393abcc9bb5ce91e5
Carrington Jones <carrington.jones47@gmail.com>
2021-05-25 09:10:14 -0500

    Completed morning warmup

--- js/warmups.js
+++ js/warmups.js
@@ -1,19 +1,26 @@
-// Fizzbuzz
+// Fizzbuzz 05.24.21
 
-function fizzbuzz(number) {
-    for (var i = 1; i < number + 1; i++) {
-        if (i % 3 === 0 && i % 5 === 0) {
-            console.log("FizzBuzz");
-        }
-        else if (i % 5 === 0) {
-             console.log("Buzz")
-        }
-        else if (i % 3 === 0) {
-             console.log("Fizz")
-        } else {
-             console.log(i);
-        }
-    }
+// function fizzbuzz(number) {
+//     for (var i = 1; i < number + 1; i++) {
+//         if (i % 3 === 0 && i % 5 === 0) {
+//             console.log("FizzBuzz");
+//         }
+//         else if (i % 5 === 0) {
+//              console.log("Buzz")
+//         }
+//         else if (i % 3 === 0) {
+//              console.log("Fizz")
+//         } else {
+//              console.log(i);
+//         }
+//     }
+// }
+//
+// console.log(fizzbuzz(30))
+
+//Seven-"Write a function in JavaScript that returns the number 7" 05.25.21
+function seven () {
+    return 7
 }
+const seven2 = () => 7;
 
-console.log(fizzbuzz(30))
